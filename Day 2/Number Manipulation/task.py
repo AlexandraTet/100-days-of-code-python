2366f44ceabe3658d80a93a59ab43e7ba54620cf
Alexandra <atrobber@gmail.com>
2026-09-14 23:11:11 +0300

    Day 2: data types, type error, checking and conversion, mathematical operations, number manipulation, tip calculator project

---
+++ Day 2/Number Manipulation/task.py
@@ -0,0 +1,20 @@
+# # bmi = 84 / 1.65 ** 2
+# # print(int(bmi))
+# # print(round(bmi))
+#
+# score = 5
+# score /= 2
+# print(score)
+#
+
+name = input("Enter your name: ")
+print(f'Hello, {name}')
+
+name = input("Enter your name: ")
+print("Hello, " + name)
+
+age = 12
+print(f'You are {age} years old')
+
+age = 12
+print('You are ' + age + ' years old')
